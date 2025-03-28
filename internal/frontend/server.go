28f59c7e18fd5fa912546a5765b79cbbd0dcb508
NEKO-CwC <neeeeeeeeekooooooooo@gmail.com>
2025-03-28 18:24:18 +0800

    fix: replace fmt into log

--- internal/frontend/server.go
+++ internal/frontend/server.go
@@ -615,7 +615,7 @@ func (s *Server) renderErrorPage(ctx context.Context, status int, templateName s
 func (s *Server) servePage(ctx context.Context, w http.ResponseWriter, templateName string, page any) {
 	defer stats.Elapsed(ctx, "servePage")()
 
-	fmt.Println("servePage", templateName, page)
+	log.Infof(ctx, "servePage %s %+v", templateName, page)
 
 	buf, err := s.renderPage(ctx, templateName, page)
 	if err != nil {
