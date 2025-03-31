85c5bd5790e2f17b329ab2cd682573398a7f3d64
NEKO-CwC <neeeeeeeeekooooooooo@gmail.com>
2025-03-31 15:40:53 +0800

    fix: QueryRow error resolve

--- internal/postgres/llpkg.go
+++ internal/postgres/llpkg.go
@@ -20,25 +20,29 @@ func (e *LLPkgConfigNotFoundError) Error() string {
 
 // GetLLPkgConfig retrieves the LLPkgConfig file for a given module path and version.
 func (db *DB) GetLLPkgConfig(ctx context.Context, modulePath, version string) (*llpkgcfg.LLPkgConfig, error) {
-	var err error
-
 	if !llpkg.IsOfficialLLPkgModule(modulePath) {
 		return nil, &LLPkgConfigNotFoundError{modulePath, version}
 	}
 
 	var llpkgConfig *llpkgcfg.LLPkgConfig
 	var content json.RawMessage
-	err = db.db.QueryRow(ctx, `
+	err := db.db.QueryRow(ctx, `
         SELECT llpkg_config
         FROM modules
         WHERE module_path = $1 AND version = $2`,
 		modulePath, version).Scan(&content)
 
-	err = json.Unmarshal(content, &llpkgConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(content, &llpkgConfig); err != nil {
+		return nil, fmt.Errorf("unmarshal llpkg config: %v", err)
+	}
 
 	if content == nil {
 		return nil, &LLPkgConfigNotFoundError{modulePath, version}
 	}
 
-	return llpkgConfig, err
+	return llpkgConfig, nil
 }
